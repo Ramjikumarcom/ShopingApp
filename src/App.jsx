7151a51be2a17d0332e7de8cc9a587d1a5f49f87
unknown <ramjik708@gmail.com>
2025-01-26 00:40:29 +0530

    file is commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,47 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Shop from './Pages/Shop'
-import ShopCategory from './Pages/ShopCategory'
-import Product from './Pages/Product'
-import Cart from './Pages/Cart'
-import LoginSignup from './Pages/LoginSignup'
-import Footer from './Components/Footer/Footer'
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
 
-import men_banner from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kid_banner from './Components/Assets/banner_kids.png'
-import AboutCompany from './Components/AboutCompany/AboutCompany'
-import Offices from './Components/Offices/Offices'
-import Help from './Components/Help/Help'
-import Payment from './Components/Payment/Payment'
-import Profile from './Components/Profile/Profile'
-import PromoCodeData from './Components/PromoCodeData/PromoCodeData'
-import CheckVerification from './Components/CheckVerification/CheckVerification'
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
+import AboutCompany from './Components/AboutCompany/AboutCompany';
+import Offices from './Components/Offices/Offices';
+import Help from './Components/Help/Help';
+import Payment from './Components/Payment/Payment';
+import Profile from './Components/Profile/Profile';
+import CheckVerification from './Components/CheckVerification/CheckVerification';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path='/' element={<Shop></Shop>}></Route>
-          <Route path='/men' element={<ShopCategory banner={men_banner} category={'men'}></ShopCategory>}></Route>
-          <Route path='/women' element={<ShopCategory category={'women'} banner={women_banner}></ShopCategory>}></Route>
-          <Route path='/kids' element={<ShopCategory category={'kid'} banner={kid_banner}></ShopCategory>}></Route>
-          <Route path='/product' element={<Product></Product>}>
-            <Route path=':productId' element={<Product></Product>}></Route>
-          </Route>
-          <Route path='/cart' element={<Cart></Cart>}></Route>
-          <Route path='/login' element={<LoginSignup></LoginSignup>}></Route>
-          <Route path='/AboutCompany' element={<AboutCompany></AboutCompany>}></Route>
-          <Route path='/offices' element={<Offices></Offices>}></Route>
-          <Route path='/help' element={<Help></Help>}></Route>
-          <Route path='/payment' element={<Payment></Payment>}></Route>
-          <Route path='/profile' element={<Profile></Profile>}></Route>
-          <Route path='/CheckVerification' element={<CheckVerification></CheckVerification>}></Route>
-        </Routes>
-
-        <Footer></Footer>
-
-
-      </BrowserRouter>
-
-
-    </>
-  )
+    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/ShopingApp' : '/'}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/men" element={<ShopCategory banner={men_banner} category="men" />} />
+        <Route path="/women" element={<ShopCategory banner={women_banner} category="women" />} />
+        <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route path="/AboutCompany" element={<AboutCompany />} />
+        <Route path="/offices" element={<Offices />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/CheckVerification" element={<CheckVerification />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
